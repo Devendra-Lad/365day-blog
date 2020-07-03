86333c59d56d9e0c47c849313ce7b01a78e7028e
DeVeN <devenlad@amazon.com>
2020-07-02 20:52:37 -0700

    Updates

---
+++ precache-manifest.642f7314d03f5790f3d1d48446e05e7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cb9218cbe20b82579912fa31eb4d78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc4e57d4bdc8f9c7207",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85ed60846e2f1bb2a415",
+    "url": "/static/js/2.8bb3fd1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8bb3fd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc4e57d4bdc8f9c7207",
+    "url": "/static/js/main.d68b269d.chunk.js"
+  },
+  {
+    "revision": "958ea144cbbca16ae7c2",
+    "url": "/static/js/runtime-main.2e600cbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
